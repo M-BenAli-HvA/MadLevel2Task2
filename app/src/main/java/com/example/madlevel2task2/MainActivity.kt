ea819883d2e8f4a8f5b20d42b09a97ac4fec9395
M-BenAli-HvA <mohamed.ben.ali@hva.nl>
2020-09-03 21:12:26 +0200

    Initial commit

---
+++ app/src/main/java/com/example/madlevel2task2/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.madlevel2task2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.madlevel2task2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+
+    private fun initViews() {
+
+    }
+
+
+}
